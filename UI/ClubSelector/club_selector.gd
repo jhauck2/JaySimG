19d63c08b59c55f4572b44f67535ce096d8c783e
Jake <jghauck17@gmail.com>
2025-06-29 12:33:53 -0500

    Added a club selector

---
+++ UI/ClubSelector/club_selector.gd
@@ -0,0 +1,29 @@
+extends Control
+
+var clubs := ["Dr", "3w", "Hy", "4i", "5i", "6i", "7i", "8i", "9i", "Pw", "Sw", "Lw"]
+var club_index := 0
+
+signal club_selected(club: String)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_left_select_pressed() -> void:
+	club_index -= 1
+	if club_index < 0:
+		club_index = clubs.size() - 1
+	
+	emit_signal("club_selected", clubs[club_index])
+
+
+func _on_right_select_pressed() -> void:
+	club_index = (club_index + 1) % clubs.size()
+	
+	emit_signal("club_selected", clubs[club_index])
