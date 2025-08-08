ca1e4627e56e6f51412a0ba63f1eac69455ca6d1
Jake <jghauck17@gmail.com>
2025-08-08 08:19:13 -0500

    Added main menu and Scene Manager per issue 8

---
+++ UI/MainMenu/main_menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	SceneManager.current_scene = self
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_range_pressed() -> void:
+	SceneManager.change_scene("res://Courses/Range/range.tscn")
+
+
+func _on_courses_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
